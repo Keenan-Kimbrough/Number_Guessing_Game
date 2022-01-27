8310b4e80006a76c5cb24455981e5fec8a7d55e9
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-01-26 20:18:40 -0500

    added Javascript

---
+++ script.js
@@ -0,0 +1,44 @@
+const secretNumber = ( Math.random() * 20 ) + 1;
+
+let score = 20;
+
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {  
+      const guess = Number(document.querySelector('.guess').value);
+
+
+    console.log(guess, typeof guess);
+    
+    document.querySelector('.check').addEventListener('click', function () { console.log(' Hi Keenan ')})
+
+//guess is not a numebrif 
+
+
+(!guess) { document.querySelector('.message').textContent = ' NO Number! ';
+    score--;
+
+document.querySelector('.score').textContent = score}
+
+// when player wins
+else if (guess === secretNumber ) 
+{    document.querySelector('.message').textContent = ' You have guessed the secret number'    
+
+
+if ( score > highscore ) {
+        highscore = score
+        document.querySelector('.highscore').textContent = highscore
+    }
+}// when guess is too highelse if ( guess> secretNumber) {    if (score >1)  {
+
+
+
+        document.querySelector('.message').textContent = ' you guessed to high';  
+          score--;    document.querySelector('.score').textContent = score;
+}
+else {
+ document.querySelector('.message').textContent = ' You completely losted the game';
+}
+}
+//when the guess is to lowelse if ( guess < secretNumber) {    document.querySelector('.message').textContent = ' To low ';   score--;    document.querySelector('.score').textContent = score;
+}
